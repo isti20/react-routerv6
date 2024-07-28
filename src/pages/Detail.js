e2ffb1716bdcf733c40ca0dd189a0e494f20760c
iis istiqomah <iisistiqomah20@gmail.com>
2024-07-28 13:16:23 +0700

    Reading URL Params: useParams

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,25 @@
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+
 const Detail = () => {
+  const { id } = useParams();
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
     <div>
       <h1>Detail page</h1>
+      <h2>Params id: {id}</h2>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setUsers(data);
       })
       .catch((err) => console.log(err));
