1f1c76c5c7b28c04bf480255cbc347a6a8092505
iis istiqomah <iisistiqomah20@gmail.com>
2024-07-28 12:58:57 +0700

    Navigation: useNavigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import Login from "../src/pages/Login";
 import Detail from "../src/pages/Detail";
@@ -7,9 +7,6 @@ import Detail from "../src/pages/Detail";
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to={"/"}>Home</Link> | <Link to={"/login"}>Login</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Detail />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
 const Home = () => {
-    return (
-      <div>
-        <h1>Home page</h1>
-      </div>
-    );
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleGoToDetail = (id) => {
+    navigate(`/${id}`);
   };
-  
-  export default Home;
-  
+
+  return (
+    <div>
+      <h1>Home page</h1>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} | {" "}
+              <button onClick={() => handleGoToDetail(user.id)}>
+                Go to detail
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
